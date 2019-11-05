7b61c935cbdae93bd7f9171c8a5ee906fdf3bd48
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-11-05 11:59:12 +0100

    add config

---
+++ config.mk
@@ -0,0 +1,7 @@
+PYTHON_LIB=python3.7m
+GT_PATH=/home/lukas/documents/work/gridtools5/include
+GTCLANG_PATH=/home/lukas/documents/work/dawn/gtclang/src/
+PYTHON_PATH=/usr/include/python3.7m
+PYBIND11_PATH=/home/lukas/packages/pybind11/2.2.3/include
+BOOST_PATH=/home/lukas/packages/boost/1.67/include
+CC=g++
