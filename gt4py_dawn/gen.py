a90f4874b5c99f3ca55af41ad70c352eb40f7f76
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-11-07 10:08:07 +0100

    constants

--- gt4py_dawn/gen.py
+++ gt4py_dawn/gen.py
@@ -8,11 +8,13 @@ assert len(sys.argv) == 3
 env = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
 template = env.get_template(sys.argv[1])
 
+# constants are not in param in the dawn repository, because they need to be in the SIR only
+# In the SIR, we can add them to the globals, and set them by compiling with the `--config=file.json`
+# option and provide a file which defines the globals.
 params = dict(
     stencil_unique_name="copy_stencil",
     module_name="gt4py_dawn_computation",
     backend="x86",
-    constants=dict(),
     arg_fields=[
         dict(name="f_out"),
         dict(name="f_in"),
